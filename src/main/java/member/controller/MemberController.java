b1104eae893fa9b419479403509eea3494ef3cd4
junh0y <y.junh0y@gmail.com>
2022-02-11 18:23:48 +0900

    멤버 관련 클래스 업로드

---
+++ src/main/java/member/controller/MemberController.java
@@ -0,0 +1,35 @@
+package member.controller;
+
+import javax.servlet.http.HttpServletRequest;	
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import member.command.MemberCommandImpl;
+import member.command.MemberViewCommand;
+import member.model.MemberDAO;
+import member.model.MemberDTO;
+ 
+@Controller
+public class MemberController
+{
+	@Autowired
+	private MemberDAO dao;
+
+	MemberViewCommand command = null;
+	
+	@Autowired
+	MemberViewCommand memberViewCommand;
+	
+	@RequestMapping("/member/view.do")
+	public String memberView(Model model, HttpServletRequest req, MemberDTO memberDTO)
+	{ 
+		model.addAttribute("req", req);
+		command = memberViewCommand;
+		command.execute(model);
+
+		return "Member/MemberInfoEdit";
+	}	
+}
