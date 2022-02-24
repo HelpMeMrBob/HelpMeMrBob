7010305a2c6f96045186821e60ffe875fc24f516
sonjy2717 <sonjy2717@naver.com>
2022-02-24 12:57:14 +0900

    지도 즐겨찾기 기능 완료

---
+++ src/main/java/infomation/model/InfomationDAOInter.java
@@ -0,0 +1,14 @@
+package infomation.model;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface InfomationDAOInter {
+	public ArrayList<InfomationDTO> keyword(String search);
+	public int favorite(FavoriteDTO favoriteDTO);
+	public ArrayList<FavoriteDTO> favoriteList(String id);
+	public int favoriteModify(String memo, String idx);
+	public int favoriteDelete(String idx);
+}
