019a0f2a32ea27fa0cc013175c08a750258bbfa8
ybj0096 <ybj0096@naver.com>
2022-02-25 18:49:36 +0900

    집갈거임

--- src/main/java/board/controller/MybatisController.java
+++ src/main/java/board/controller/MybatisController.java
@@ -25,193 +25,93 @@ import util.PagingUtil;
 public class MybatisController {
 
 	/*
-	Mybatis를 사용하기 위해 빈을 자동주입 받는다.
-	servlet-context.xml에서 생성함
+	 * Mybatis를 사용하기 위해 빈을 자동주입 받는다. servlet-context.xml에서 생성함
 	 */
 	private SqlSession sqlSession;
+
 	@Autowired
 	public void setSqlSession(SqlSession sqlSession) {
-		this.sqlSession=sqlSession;
+		this.sqlSession = sqlSession;
 		System.out.println("Mybatis 사용 준비 끝");
 	}
-	//댓글ㅎㅎ
-	@RequestMapping("/mybatis/list.do")
-	public String list(Model model, HttpServletRequest req) {
-		//댓 갯수 카운트
-		int totalRecordCount=sqlSession.getMapper(MybatisDAOImpl.class)
-				.getTotalCount();
-		//페이지 처리를 위한 설정값
-		int pageSize=4;//한 페이지당 출력할 게시물의 갯수
-		int blockPage=2;//한 블럭당 출력할 페이지 번호의 갯수
-		//전체 페이지 수 계산
-		int totalPage=(int)Math.ceil((double)totalRecordCount/pageSize);
-		//현재페이지 번호 설정
-		//페이지 번호가 null이거나 빈값인 경우 1페이지로 설정한다.
-		int nowPage=(req.getParameter("nowPage")==null || req.getParameter("nowPage").equals(""))
-				? 1:Integer.parseInt(req.getParameter("nowPage"));
-		//해당 페이지에 출력할 게시물의 구간을 계산한다.
-		int start=(nowPage-1)*pageSize+1;
-		int end=nowPage*pageSize;
-		
-		model.addAttribute("idx",req.getParameter("idx"));
-		String idx = req.getParameter("idx");
-		
-		
-		
-		/*
-		 서비스 역할의 인터페이스의 추상메서드를 호출하면 mapper가 동작됨
-		 전달된 파라미터는 #{param1}과 같이 순서대로 사용한다.
-		 */
-		ArrayList<BoardReplyVO> lists=
-				sqlSession.getMapper(MybatisDAOImpl.class).listPage(start, end, idx);
-		String pagingImg=
-				PagingUtil.pagingImg(totalRecordCount,pageSize, blockPage, nowPage,
-						req.getContextPath()+"/mybatis/list.do?");
-		model.addAttribute("pagingImg",pagingImg);
-		
-		for(BoardReplyVO dto: lists)
-		{
-			String temp=dto.getContents().replace("\r\n", "<br/>");
-			dto.setContents(temp);
-		}
-		model.addAttribute("lists",lists);
-		
-		System.out.println("댓글의 idx를 받아오는걸까요?"+idx);
-		
-		return "main/blog-single2";
-	}
-	 
-	//글쓰기 페이지 매핑
-	@RequestMapping("/mybatis/write.do")
-	public String write(Model model, HttpSession session, HttpServletRequest req) {
-		/*
-		 매핑된 메서드 내에서 session 내장객체를 사용하기 위해 매개변수로 선언해준다.
-		 */
-		//session 영역에 해당 속성이 없다면 로그아웃 상태이므로 로그인페이지로 이동한다.
-		if(session.getAttribute("siteUserInfo")==null) {
-			/*
-			현재상태는 글쓰기를 위해 버튼을 클릭했으므로, 만약 로그인이 완료된다면
-			글쓰기 페이지로 이동하는 것이 좋다. 따라서 backUrl이라는 파라미터에
-			쓰기페이지의 View 경로를 붙여서 리다이렉트 시킨다.
-			 */
-			model.addAttribute("backUrl","07Mybatis/write");
-			return "redirect:login.do";
-		}
-		//로그인이 완료된 상태라면 쓰기 페이지로 진입한다.
-		return "07Mybatis/write";
-	}
-	//로그인 페이지 매핑 
-	@RequestMapping("/mybatis/login.do")
-	public String login(Model model) {
-		return "07Mybatis/login";
-	}
-	//로그인처리(session객체 사용)
-	@RequestMapping("/mybatis/loginAction.do")
-	public ModelAndView loginAction(HttpServletRequest req, HttpSession session) {
-		//폼값으로 전송된 id, pass를 매개변수로 전달하여 Mapper 호출
-		MemberVO vo=sqlSession.getMapper(MybatisMemberImpl.class)
-				.login(req.getParameter("id"), req.getParameter("pass"));
-		ModelAndView mv=new ModelAndView();
-		if(vo==null) {
-			//로그인에 실패한 경우(정보 불일치 등)
-			mv.addObject("LoginNG","아이디/패스워드가 틀렸습니다.");
-			//로그인 페이지로 다시 돌아간다.
-			mv.setViewName("07Mybatis/login");
-			return mv;
-		}
-		else {
-			//로그인에 성공한 경우 세션영역에 MemberVO객체를 저장한다.
-			session.setAttribute("siteUserInfo", vo);
-		}
-		//글쓰기 페이지로의 진입에 실패한 경우라면 backUrl을 통해 글쓰기 페이지로 이동시킨다.
-		String backUrl=req.getParameter("backUrl");
-		if(backUrl==null|| backUrl.equals("")) {
-			//디폴트로 이동할 페이지는 로그인이다.
-			mv.setViewName("07Mybatis/login");
-		}
-		else {
-			mv.setViewName(backUrl);
-		}
-		return mv;
-	}
 	
-	//댓글쓰기
-	@RequestMapping(value="/replyWrite.do",
-			method=RequestMethod.POST)
+
+
+	// 댓글쓰기
+	@RequestMapping(value = "/replyWrite.do", method = RequestMethod.POST)
 	public String writeAction(Model model, HttpServletRequest req, HttpSession session, BoardReplyVO boardReplyVO) {
-		//로그인확인
-		if(session.getAttribute("siteUserInfo")==null) {
-			//만약 세션이 끊어졌다면 로그인페이지로 이동한다.
+		// 로그인확인
+		if (session.getAttribute("siteUserInfo") == null) {
+			// 만약 세션이 끊어졌다면 로그인페이지로 이동한다.
 			return "redirect:login.do";
 		}
-		model.addAttribute("req",req);
-		model.addAttribute("BoardReplyVO",boardReplyVO);
-		
-		model.addAttribute("idx",req.getParameter("idx"));
-		model.addAttribute("nowPage",req.getParameter("nowPage"));
-		
+		model.addAttribute("req", req);
+		model.addAttribute("BoardReplyVO", boardReplyVO);
+
+		model.addAttribute("idx", req.getParameter("idx"));
+		model.addAttribute("nowPage", req.getParameter("nowPage"));
+
 		String idx = req.getParameter("idx");
 		System.out.println(idx);
-		
-		//insert문을 실행시 입력에 성공한 행의 갯수가 정수로 반환된다 얘 머임.
-		  int result= sqlSession.getMapper(MybatisDAOImpl.class).write(idx,
-		  req.getParameter("name"), req.getParameter("contents"),
-		  ((MemberVO)session.getAttribute("siteUserInfo")).getId());
-		  System.out.println("입력한 결과"+result);
-				
-				return "redirect:reviewView.do";
-			}
-	//수정페이지 진입하기
+
+		// insert문을 실행시 입력에 성공한 행의 갯수가 정수로 반환된다 얘 머임.
+		int result = sqlSession.getMapper(MybatisDAOImpl.class).write(idx, req.getParameter("name"),
+				req.getParameter("contents"), ((MemberVO) session.getAttribute("siteUserInfo")).getId());
+		System.out.println("입력한 결과" + result);
+
+		return "redirect:reviewView.do";
+	}
+
+	// 수정페이지 진입하기
 	@RequestMapping("/modify.do")
-	public String modify(Model model, HttpServletRequest req,
-			HttpSession session) {
-		//수정페이지 진입시에도 로그인 확인해야함
-		if(session.getAttribute("siteUserInfo")==null) {
+	public String modify(Model model, HttpServletRequest req, HttpSession session) {
+		// 수정페이지 진입시에도 로그인 확인해야함
+		if (session.getAttribute("siteUserInfo") == null) {
 			return "redirect:login.do";
 		}
-		
+
 		/*
-		 파라미터를 전달하는 4번째 방법으로 DTO(혹은 VO) 객체에 파라미터를 
-		 저장한 후 Mapper로 전달한다.
+		 * 파라미터를 전달하는 4번째 방법으로 DTO(혹은 VO) 객체에 파라미터를 저장한 후 Mapper로 전달한다.
 		 */
-		ParameterDTO parameterDTO =new ParameterDTO();
-		//일련번호 저장
+		ParameterDTO parameterDTO = new ParameterDTO();
+		// 일련번호 저장
 		parameterDTO.setBoard_idx(req.getParameter("idx"));
-		//사용자 아이디 저장
-		parameterDTO.setUser_id(((MemberVO)session.getAttribute("siteUserInfo")).getId());
-		
-		//view() 메서드로 앞에서 저장된 DTO객체를 매개변수로 전달한다.
-		BoardReplyVO dto=sqlSession.getMapper(MybatisDAOImpl.class).view(parameterDTO);
-		
-		model.addAttribute("dto",dto);
-		return "07Mybatis/modify";
+		// 사용자 아이디 저장
+		parameterDTO.setUser_id(((MemberVO) session.getAttribute("siteUserInfo")).getId());
+
+		// view() 메서드로 앞에서 저장된 DTO객체를 매개변수로 전달한다.
+		BoardReplyVO dto = sqlSession.getMapper(MybatisDAOImpl.class).view(parameterDTO);
+
+		model.addAttribute("dto", dto);
+		return "main/contact5";
 	}
-	//수정처리
-	@RequestMapping("/mybatis/modifyAction.do")
-	public String modifyAction(HttpSession session, BoardReplyVO boardReplyVO) {
-		//수정처리에서 전송된 폼값은 커맨드객체를 통해 한꺼번에 받는다.
-		
-		//수정처리 전 로그인 확인 
-		if(session.getAttribute("siteUserInfo")==null) {
+
+	// 수정처리
+	@RequestMapping("/modifyAction.do")
+	public String modifyAction(HttpSession session, BoardReplyVO boardReplyVO, HttpServletRequest req) {
+		// 수정처리에서 전송된 폼값은 커맨드객체를 통해 한꺼번에 받는다.
+		String idx = req.getParameter("idx");
+		// 수정처리 전 로그인 확인
+		if (session.getAttribute("siteUserInfo") == null) {
 			return "redirect:login.do";
 		}
-		//수정처리를 위해 modify 메서드 호출
-		int applyRow=sqlSession.getMapper(MybatisDAOImpl.class).modify(boardReplyVO);
-		System.out.println("수정처리된 레코드 수:"+applyRow);
-		//방명록 게시판은 상세보기 페이지가 별도로 없으므로 리스트로 이동하면 된다.
-		return "redirect:list.do";
+		// 수정처리를 위해 modify 메서드 호출
+		int applyRow = sqlSession.getMapper(MybatisDAOImpl.class).modify(boardReplyVO);
+		System.out.println("수정처리된 레코드 수:" + applyRow);
+		// 방명록 게시판은 상세보기 페이지가 별도로 없으므로 리스트로 이동하면 된다.
+		return "redirect:reviewView.do?idx=" + idx;
 	}
-	//삭제처리
+
+	// 삭제처리
 	@RequestMapping("/mybatis/delete.do")
 	public String delete(HttpServletRequest req, HttpSession session) {
-		//로그인 확인 요거 시큐리티가 알아서 해준대 대단하징?
-		if(session.getAttribute("siteUserInfo")==null) {
+		// 로그인 확인 요거 시큐리티가 알아서 해준대 대단하징?
+		if (session.getAttribute("siteUserInfo") == null) {
 			return "redirect:login.do";
 		}
-		//삭제처리를 위해 delete() 호출
-		sqlSession.getMapper(MybatisDAOImpl.class).delete(
-				req.getParameter("idx"),
-				((MemberVO)session.getAttribute("siteUserInfo")).getId());
+		// 삭제처리를 위해 delete() 호출
+		sqlSession.getMapper(MybatisDAOImpl.class).delete(req.getParameter("idx"),
+				((MemberVO) session.getAttribute("siteUserInfo")).getId());
 		return "redirect:list.do";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/main/contact5.jsp
@@ -0,0 +1,133 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<jsp:include page="/WEB-INF/views/include/globalHeader.jsp" />
+	
+  <body class="video-bg-homepage">
+<script type="text/javascript">
+function writeValidate(f)
+{
+	if(f.name.value==""){
+		alert("작성자 이름을 입력하세요");
+		f.name.focus();
+		return false;
+	}
+	if(f.contents.value==""){
+		alert("내용을 입력하세요");
+		f.contents.focus(); 
+		return false;
+	} 
+}
+</script>
+	
+	
+    <!-- =================== SITE HEADER BEGINS ============================= -->
+	
+    <jsp:include page="/WEB-INF/views/include/header.jsp" />
+	
+    <!-- =================== SITE HEADER ENDS ============================= -->
+	
+	
+    <!-- =================== MAIN SECTION BEGINS ============================= -->
+	
+    <main>
+	
+      <!-- BANNER SECTION STARTS -->
+      <section class="banner full-width">
+		
+        <div class="container">
+			
+          <div class="banner__content">
+			
+            <div class="section-heading">
+              <span class="section-intro"></span>
+              <h1>Contact</h1>
+            </div><!-- .section-heading ends -->
+			
+            <div class="breadcrumb">
+              <div class="breadcrumb__home--link"><a href="index.html">Home</a></div>
+              <span>/</span>
+              <div class="breadcrumb__current--page-link">Contact</div>
+            </div><!-- .breadcrumb ends -->
+			
+          </div><!-- .banner__content ends -->
+			
+        </div><!-- .container ends -->
+		
+      </section><!-- .banner ends -->
+      <!-- BANNER SECTION ENDS -->
+		
+      <!-- CONTACT FORM STARTS -->
+      <section class="contact default-section-spacing">
+		
+        <div class="container">
+			
+          <form name="writeFrm" method="post" 
+          action="<c:url value="./modifyAction.do" />" class="form contact__form"
+          onsubmit="return writeValidate(this);">
+			<input type="hidden" name="idx" value="${dto.idx }"/>
+			<input type="hidden" name="id" value="${sessionScope.siteUserInfo.id}"/>
+			<div class="row">
+				
+				
+              <div class="flex-md-12 mar-b-md-2">
+				
+                <div class="form__group">
+                  <label for="subject" class="form__label">작성자 <span class="color-danger">*</span></label>
+                  <input type="text" id="subject" class="form__input" name="name" value="${dto.name }">
+                </div><!-- .form__group ends -->
+				
+              </div><!-- .flex-* ends -->
+				
+              <div class="flex-md-12 mar-b-md-2">
+				
+                <div class="form__group">
+                  <label for="message-2" class="form__label">내용 <span class="color-danger">*</span></label>
+                  <textarea name="contents" id="message-2" class="form__textarea form__input">${dto.contents }</textarea>
+                </div><!-- .form__group ends -->
+				
+              </div><!-- .flex-* ends -->
+				
+              <div class="flex-md-12">
+				
+                <button class="button--primary button--fill" type="submit" name="submit">수정 완료</button>
+				
+              </div><!-- .flex-* ends -->
+				
+				
+            </div><!-- .row ends -->
+			
+          </form><!-- .form ends -->
+			
+        </div><!-- .container ends -->
+		
+      </section><!-- .contact ends -->
+      <!-- CONTACT FORM ENDS -->
+	
+    </main><!-- main ends -->
+	
+    <!-- =================== MAIN SECTION ENDS ============================= -->
+	
+	
+    <!-- SCROLL BACK TO TOP BEGINS -->
+    <div class="scroll-to-top"><i class="ri-arrow-up-line"></i></div>
+    <!-- SCROLL BACK TO TOP ENDS -->
+	
+    <!-- =================== ALL MODALS ============================= -->
+	
+    <jsp:include page="/WEB-INF/views/include/search.jsp" />
+	
+    <!-- =================== ALL MODALS END ============================= -->
+	
+	
+    <!-- =================== SITE FOOTER BEGINS ============================= -->
+	
+    <jsp:include page="/WEB-INF/views/include/footer.jsp" />
+	
+    <!-- =================== SITE FOOTER ENDS ============================= -->
+	
+	<jsp:include page="/WEB-INF/views/include/jquery.jsp" />
+	
+  </body>
+	
+</html>
