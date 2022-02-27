8fd97dc32a2dad778ddab8a8452e09eb03762d9d
popovely <popovely@gmail.com>
2022-02-28 00:43:22 +0900

    월드컵 템플릿 / 메인 페이지 수정

--- src/main/java/recommand/model/RecommandDAO.java
+++ src/main/java/recommand/model/RecommandDAO.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
-import board.model.ParameterDTO;
-
 @Service
 public interface RecommandDAO {
 	// 월드컵에서 가져와야 할 데이터 (food:음식이름, image:이미지 이름, recomndCnt:월드컵에서 우승한 횟수)
-	public ArrayList<RecommandDTO> listData(RecommandParameterDTO recommandParameterDTO);
+	public ArrayList<RecommandDTO> listData(RecommandDTO RecommandDTO);
 }
