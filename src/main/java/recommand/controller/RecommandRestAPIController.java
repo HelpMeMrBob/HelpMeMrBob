91bb52e34b8e91f4b979ea11d77dcfc1ccfd8848
popovely <popovely@gmail.com>
2022-02-28 16:14:56 +0900

    RecommandRestAPIControllter 수정했습니당 ㅠㅠㅠ흑흑

--- src/main/java/recommand/controller/RecommandRestAPIController.java
+++ src/main/java/recommand/controller/RecommandRestAPIController.java
@@ -3,6 +3,7 @@ package recommand.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,41 +23,60 @@ public class RecommandRestAPIController {
 	
 	@RequestMapping("/worldcupData.do")
 	@ResponseBody
-	public ArrayList<RecommandDTO> worldcupList(RecommandDTO recommandDTO) {
+	public ArrayList<RecommandDTO> worldcupList(Model model, RecommandDTO recommandDTO) {
 		
-		ArrayList<RecommandDTO> record = sqlSession3.getMapper(RecommandDAO.class)
+		ArrayList<RecommandDTO> menu64 = sqlSession3.getMapper(RecommandDAO.class)
 				.listData(recommandDTO);
-		ArrayList<RecommandDTO> result = new ArrayList<RecommandDTO>();
-		int[] num = new int[64];
 		
-		// 중복되지 않는 64개의 난수 생성(좋은 코드는 아님..)
+		System.out.println("menu64 개수 : "+ menu64.size());
+		
+		return menu64;
+	}
+	
+	public void randomCreate(int[] arrParam) {
+		
+		//난수생성을 위한 씨드설정
+		Random rnd = new Random();
+		rnd.setSeed(System.currentTimeMillis());
+
+		/*
+		난수생성 방법1:
+			1]난수 10개를 우선 생성한후 배열에 담아준다.
+			2]배열 전체를 대상으로 중복확인을 한다.
+			3]만약 중복되는 요소가 발견되면 다시 1번으로 돌아가서 난수를
+			생성한다.
+			4]중복되는 요소가 없다면 함수를 종료하고 메인으로 돌아간다.
+		 */
 		while(true) {
-	        boolean flag = true;
-	        
-	        for (int i = 0; i < num.length; i++) {
-	        	int temp = (int)(Math.random() * 191) + 1;
-	            num[i] = temp;
-	            for (int j = 1; j < num.length; j++) {
-	                if (num[i] == num[j] && i != j) {
-	                    flag = false;
-	                    break;
-	                }
-	            }
-	            if (!flag) break;
-        	}
-        	if (flag) break;
-    	}
-		
-		for (int i = 0; i < record.size(); i++) {
-			for (int j = 0; j < num.length; j++) {
-				if (Integer.parseInt(record.get(i).getIdx()) == num[j]) {
-					result.add(record.get(j));
+
+			//1.난수10개 생성후 배열담기
+			for(int i=0 ; i<arrParam.length ; i++) {
+				arrParam[i] = rnd.nextInt(99) + 1;//1~99까지의 정수 생성
+			}
+
+			//2.중복확인
+			boolean rndFlag = false;/*중복체크를 위한 변수(false라면 중복된
+				난수가 없는경우이고, 중복된 난수가 발견된다면 true로
+				값을 변경할것임 */
+
+			for(int i=0 ; i<arrParam.length ; i++) {
+				for(int j=0 ; j<arrParam.length ; j++) {
+					/*
+					비교의 조건은 인덱스i와 인덱스j가 서로 다를때이다.
+					인덱스가 동일하다면 같은숫자일테니까...
+					 */
+					if(i!=j && arrParam[i]==arrParam[j]) {
+						//중복된 값이 발견된다면 true로 값을 변경
+						rndFlag = true;
+					}
 				}
 			}
-		}
-		
-		return result;
-	}
+
+			//3.중복된값이 없다면 break로 while루프 탈출하기
+			if(rndFlag==false) break;
+
+		}//end of while
+	}//
 	
 	// 룰렛 화면 매핑
 	@RequestMapping("/roulette.do")
